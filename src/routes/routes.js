8b5c348616258876b0b28388a4809d48727a5dab
Matthew Snyder <matthew.d.snyder.ii@gmail.com>
2020-06-22 16:13:08 -0500

    Completed module

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 import { addNewProduct } from '../controllers/controllers';
 
 const routes = (app) => {
-    app.route('/products').post(addnewProduct);
+    app.route('/products')
+        .post(addnewProduct);
 }
 
 export default routes;
